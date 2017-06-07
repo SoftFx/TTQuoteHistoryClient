20d71c261fd0d1c5e08a869f5bbded3f8e1bd75d
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-06-07 16:14:55 +0300

    update install.R

--- rTTQuoteHistory/install.R
+++ rTTQuoteHistory/install.R
@@ -10,4 +10,5 @@ installRPackage <- function(url){
 }
 if(!require(rClr) )
     installRPackage("https://github.com/SoftFx/rFdk-/raw/master/Lib/RClr/rClr_0.7-4.zip");
+require(devtools)
 install_github("SoftFx/TTQuoteHistoryClient",subdir = "rTTQuoteHistory/R")
